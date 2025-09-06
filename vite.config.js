d56f305fb259d79f579da9538e4896e972ce7698
Angleedas <angleedas916@gmail.com>
2025-09-06 11:19:20 +0300

    deploy setup

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  base: "/portfolio/", 
+
 })
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+// })
 // /** @type {import('tailwindcss').Config} */
 // export default {
 //   content: [
