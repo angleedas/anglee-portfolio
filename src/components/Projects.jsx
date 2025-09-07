e823c43f1ba6420e7070f107d1da0206ccfb078d
angleedas <angleedas916@gmail.com>
2025-09-07 12:46:52 +0300

    Renamed assets (removed spaces) to fix 404 issue

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,15 +1,15 @@
 // components/Projects.js
 import React from "react";
-import videoo from "./assets/videoo.mp4";
-import video from "./assets/video.mp4";
+import video1 from "./assets/video1.mp4";
+import video2 from "./assets/video2.mp4";
 import image1 from "./assets/image1.jpeg";
 import image2 from "./assets/image2.jpeg";
 
 const projects = [
-  { type: 'video', src: videoo, desc: 'A fun Flappy Bird clone built with HTML, CSS, and JavaScript where players control a bird and navigate through moving pipes. The game features smooth animations, scoring, and game-over mechanics for an engaging experience.', reverse: false },
-  { type: 'video', src: video, desc: 'A simple calculator built with HTML, CSS, and JavaScript that performs basic arithmetic operations. It provides a clean interface with responsive design for quick and easy calculations.', reverse: true },
-  { type: 'image', src: image1, desc: 'A fun Rock, Paper, Scissors game built with HTML, CSS, and JavaScript where players compete against the computer. The game includes real-time results, score tracking, and an interactive UI.', reverse: false },
-  { type: 'image', src: image2, desc: 'A dynamic solar system simulation built with HTML, CSS, and JavaScript, featuring orbiting planets and stars. This project visually demonstrates planetary motion with smooth animations for an engaging space experience.', reverse: true },
+  { type: 'video', src: {video1}, desc: 'A fun Flappy Bird clone built with HTML, CSS, and JavaScript where players control a bird and navigate through moving pipes. The game features smooth animations, scoring, and game-over mechanics for an engaging experience.', reverse: false },
+  { type: 'video', src: {video2}, desc: 'A simple calculator built with HTML, CSS, and JavaScript that performs basic arithmetic operations. It provides a clean interface with responsive design for quick and easy calculations.', reverse: true },
+  { type: 'image', src: {image1}, desc: 'A fun Rock, Paper, Scissors game built with HTML, CSS, and JavaScript where players compete against the computer. The game includes real-time results, score tracking, and an interactive UI.', reverse: false },
+  { type: 'image', src: {image2}, desc: 'A dynamic solar system simulation built with HTML, CSS, and JavaScript, featuring orbiting planets and stars. This project visually demonstrates planetary motion with smooth animations for an engaging space experience.', reverse: true },
 ];
 
 const Projects = () => (
