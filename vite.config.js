3fb9c4819d8534b9ec4120e9a32da4d029f42c6d
angleedas <angleedas916@gmail.com>
2025-09-07 13:25:12 +0300

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,12 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   base: '/anglee-portfolio/',
   plugins: [react()], 
-  base: '/anglee-portfolio/', //base: '/anglee-portfolio/',
 })
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: '/anglee-portfolio/', // 👈 important for GitHub Pages
+//   plugins: [react()],
+// })
